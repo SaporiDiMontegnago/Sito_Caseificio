fed63577decbb33d899d0fd4e16a0beaf96948d8
Eleonora Piva <piva.elenora@gmail.com>
2019-10-30 21:33:48 +0000

    Updates

---
+++ precache-manifest.7826347f364ce7cdd9d19fec9dfe8b42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43c3dec9fc675a279c17",
+    "url": "/Sito_Caseificio/static/js/runtime~main.769dbf6b.js"
+  },
+  {
+    "revision": "1457259f10def3c12d8e",
+    "url": "/Sito_Caseificio/static/js/main.6924432e.chunk.js"
+  },
+  {
+    "revision": "7fe6f3a362a2c47c946d",
+    "url": "/Sito_Caseificio/static/js/2.61fed010.chunk.js"
+  },
+  {
+    "revision": "1457259f10def3c12d8e",
+    "url": "/Sito_Caseificio/static/css/main.93674227.chunk.css"
+  },
+  {
+    "revision": "85dde52f09f416af56927aef3f54141d",
+    "url": "/Sito_Caseificio/index.html"
+  }
+];
