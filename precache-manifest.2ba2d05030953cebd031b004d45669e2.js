494e7859faf39db9b8941029367d1b6597177cd2
Eleonora Piva <piva.elenora@gmail.com>
2019-10-31 17:48:43 +0000

    Updates

---
+++ precache-manifest.2ba2d05030953cebd031b004d45669e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ca1506728a8d7347648",
+    "url": "/sito-caseificio/static/js/runtime~main.d14ee4e4.js"
+  },
+  {
+    "revision": "6eab6ea46dcbc03b71b5",
+    "url": "/sito-caseificio/static/js/main.cfdcd77a.chunk.js"
+  },
+  {
+    "revision": "d80ba709f01fd49445ea",
+    "url": "/sito-caseificio/static/js/2.c203eb2f.chunk.js"
+  },
+  {
+    "revision": "6eab6ea46dcbc03b71b5",
+    "url": "/sito-caseificio/static/css/main.93674227.chunk.css"
+  },
+  {
+    "revision": "258b3ebee38bc01a131660ce369f2311",
+    "url": "/sito-caseificio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sito-caseificio/precache-manifest.36beef5981ec0a3526e61c7638d6a99f.js"
+  "/sito-caseificio/precache-manifest.2ba2d05030953cebd031b004d45669e2.js"
 );
 
 workbox.clientsClaim();
